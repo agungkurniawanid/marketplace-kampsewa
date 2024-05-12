002eb3a3fa702da58cff9f2f69cda430e43d03f1
Agung Kurniawan <agungklewang26@gmail.com>
2024-05-13 01:14:51 +0700

    udpate api getAllProducts, getProductByIdUser, getProductByIdProduct

--- marketplace-kampsewa/app/Http/Controllers/Api/ProductController.php
+++ marketplace-kampsewa/app/Http/Controllers/Api/ProductController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Produk;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    // get product terlaris
+    public function getAllProducts() {
+        $products = Produk::all();
+        return response()->json([
+            'products' => $products,
+            'message' => 'Success'
+        ], 200);
+    }
+    public function getAllProductsByUserId($userId) {
+        $products = Produk::where('id_user', $userId)->get();
+        return response()->json([
+            'products' => $products,
+            'message' => 'Success'
+        ], 200);
+    }
+    public function getProductById($productId) {
+        $product = Produk::find($productId);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        return response()->json([
+            'product' => $product,
+            'message' => 'Success'
+        ], 200);
+    }
 }
--- marketplace-kampsewa/routes/api.php
+++ marketplace-kampsewa/routes/api.php
@@ -2,13 +2,18 @@
 
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
 Route::group(['middleware' => 'auth:sanctum'], function () {
+    // user
     Route::get('/users', [UserController::class, 'getAllUsers']);
     Route::get('/users/{identifier}', [UserController::class, 'getUserByIDOrName']);
+    // product
+    Route::get('/products', [ProductController::class, 'getAllProducts']);
+    Route::get('/products/{userId}', [ProductController::class, 'getAllProductsByUserId']);
+    Route::get('/product/{productId}', [ProductController::class, 'getProductById']);
 });
