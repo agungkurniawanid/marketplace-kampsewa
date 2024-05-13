7b09c98d59f516181df24020f5d37bdb09c227d7
Agung Kurniawan <agungklewang26@gmail.com>
2024-05-13 21:58:34 +0700

    penambahan table reset_password

---
+++ marketplace-kampsewa/app/Models/ResetPassword.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResetPassword extends Model
+{
+    use HasFactory;
+
+    protected $table = 'reset_password';
+
+    protected $fillable = [
+        'id_user',
+        'otp',
+    ];
+}
---
+++ marketplace-kampsewa/database/migrations/2024_05_13_144913_reset_password.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reset_password', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user')->nullable(false);
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->string('otp', 6)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reset_password');
+    }
+};
