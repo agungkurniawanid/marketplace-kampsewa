0738b0c235f997c5ad97c2c129fa90f9f8814c75
Agung Kurniawan <agungklewang26@gmail.com>
2024-05-25 23:11:31 +0700

    update api logout : dokumentasi kode

--- marketplace-kampsewa/app/Http/Controllers/Api/Auth/LogoutController.php
+++ marketplace-kampsewa/app/Http/Controllers/Api/Auth/LogoutController.php
@@ -8,15 +8,25 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
+    // fungsi api logout users
     public function logout(Request $request)
     {
+        // request ambil data users yang sudah login
         $user = $request->user();
+
+        // check apakah data ada jika ada jalankan blok
+        // kode didalamnya
         if ($user) {
+            // update status kolom table users terkait menjadi offline
             $user->status = 'offline';
+            // jadikan null untuk kolom time_login
             $user->time_login = null;
+            // simpan update perubahan
             $user->save();
+            // hapus token login
             $user->currentAccessToken()->delete();
         }
+        // response json dengan message berhasil logou 200 ok
         return response()->json(['message' => 'Berhasil LogOut'], 200);
     }
 }
